d8d468e842125483bfe24dbd404c953d62287c67
Ratri08 <24514@srithatpit.ac.th>
2025-03-04 10:22:58 +0700

    Add files via upload

---
+++ h1.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>html เบื้องต้น</title>
+</head>
+<body>
+    <div>
+     <h1>html เบื้องต้น(h1)</h1>
+</div>
+
+  <p>----การแสดงข้อความแบบหัวข้อ----</p>
+<div>
+    <h2>html เบื้องต้น(h2)</h2>
+    <h3>html เบื้องต้น(h2)</h3>
+    <h4>html เบื้องต้น(h2)</h4>
+    <h5>html เบื้องต้น(h2)</h5>
+    <h6>html เบื้องต้น(h2)</h6>
+</div>
+
+  <p>----การสร้างตัวหนา ตัวเอียง การขีดเส้นใต้ข้อความ----</p>
+
+  <div>
+  <b>Ratri  Khotami</b><br>
+  <i>Ratri  Khotami</i><br>
+  <u>Ratri  Khotami</u><br>
+  </div>
+
+  <p><b>---การ link---</p></b>
+
+  <div>
+    <!--link หน้าเดียว--->
+  <a href="http://www.google.co.th">Google</a><br>
+  </div>
+
+</body>
+</html>
+ 
